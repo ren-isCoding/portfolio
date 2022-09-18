ed42f2228c7d1c31a83e13d40917e815ddaf4939
renhubgit <renik2001@gmail.com>
2022-09-18 15:46:12 +0200

    added typewriter animation to about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import drawHeroCanvas from "./modules/heroCanvas"
-
+import drawBgCanvas from "./modules/bgCanvas"
+import animateTypeDelete from "./modules/typeDeleteAnimation"
+drawBgCanvas()
 drawHeroCanvas()
+animateTypeDelete()
