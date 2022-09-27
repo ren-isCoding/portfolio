c4c4f9e08fd35c2ab970082ce701240266dc9b21
renhubgit <renik2001@gmail.com>
2022-09-27 11:12:21 +0200

    added form validation js

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ import drawCanvas from "./modules/canvas"
 import animateTyping from "./modules/typingAnimation"
 import updateNavBar from "./modules/navBar"
 import updateHeroSection from "./modules/heroSection"
+import validateForm from "./modules/formValidation"
 
 drawCanvas()
 updateNavBar()
 updateHeroSection()
 animateTyping()
+validateForm()
