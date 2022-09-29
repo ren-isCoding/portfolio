f49e86a6c1b1b6ea90f48228962d1e66c2c71a51
renhubgit <renik2001@gmail.com>
2022-09-29 20:50:20 +0200

    remove path from url on scroll

--- src/js/modules/navBar.js
+++ src/js/modules/navBar.js
@@ -37,6 +37,7 @@ function updateNavBar() {
   function scrollToTop() {
     document.body.scrollTop = 0 // For Safari
     document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    history.replaceState(null, null, " ") // Remove path from url
   }
 }
 
